e329a3c67205f43cfecfadabd3e112f4876a09c6
Dylan Pugh <31519230+Dylan-Pugh@users.noreply.github.com>
2022-04-27 15:27:19 -0400

    1st pass run tests function

--- run_test.py
+++ run_test.py
@@ -1,32 +1,25 @@
 import pandas as pd
 import numpy as np
 from datetime import datetime
-
 from ioos_qc.config import QcConfig
 from ioos_qc import qartod
 
 # Water level data
 # For a fixed station in Kotzebue, AK (https://www.google.com/maps?q=66.895035,-162.566752)
-from pathlib import Path
-basedir = Path().absolute()
-libdir = basedir.parent.parent.parent
-filename = basedir.joinpath(input1)
-variable_name=input2
-
-# Load data
-data = pd.read_csv(filename, parse_dates=['time'])
-
-# Run QC
-qc = QcConfig(qc_config)
-qc_results =  qc.run(
-    inp=data[input2],
-    tinp=data['timestamp'],
-    zinp=data['z']
-)
 
 
-qc_result_pd = pd.DataFrame(qc_results["qartod"], columns=qc_results["qartod"].keys())
-result = pd.concat([data, qc_result_pd], axis=1)
-result.to_csv(input3)
+def run_tests(input_data, input_variable, qc_config, output_path):
+    # Run QC
+    qc = QcConfig(qc_config)
+    qc_results =  qc.run(
+        inp=input_data[input_variable],
+        tinp=input_data['timestamp'],
+        zinp=input_data['z']
+    )
 
-print("Done")
+
+    qc_result_pd = pd.DataFrame(qc_results["qartod"], columns=qc_results["qartod"].keys())
+    result = pd.concat([input_data, qc_result_pd], axis=1)
+    result.to_csv(output_path)
+
+    print("Done")
